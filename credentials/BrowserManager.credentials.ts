a9bde78415187cff8bc39589206765b04e7201f9
Neidn <dty3152@gmail.com>
2025-08-07 15:24:57 +0900

    feat: add BrowserManager credential type with configurable options

---
+++ credentials/BrowserManager.credentials.ts
@@ -0,0 +1,26 @@
+import {
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class BrowserManager implements ICredentialType {
+	name = 'browserManager';
+	displayName = 'Browser Manager';
+	documentationUrl = 'https://docs.n8n.io/integrations/creating-nodes/build/declarative-style-node/';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Browser Timeout (ms)',
+			name: 'browserTimeout',
+			type: 'number',
+			default: 30000,
+			description: 'Maximum time to wait for browser operations in milliseconds',
+		},
+		{
+			displayName: 'Headless Mode',
+			name: 'headless',
+			type: 'boolean',
+			default: true,
+			description: 'Whether to run browser in headless mode',
+		},
+	];
+}
